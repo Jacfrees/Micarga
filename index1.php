bc06a3a950a4f22e13c2040afdd111d9a4aef75e
Jacfrees <44384806+Jacfrees@users.noreply.github.com>
2019-03-28 16:32:23 -0500

    arreglo login y file

--- index1.php
+++
@@ -1,8 +0,0 @@
-<form action="upload.php" method="post" enctype="multipart/form-data"> 
-   
-    <b>Subir carta propiedad: </b> 
-    <br> 
-    <input name="userfile" type="file"> 
-    <br> 
-    <input type="submit" value="Enviar"> 
-    </form>  
