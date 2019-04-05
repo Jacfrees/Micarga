1a821bf718190e7cdcc26e65e90e50329c6d0a63
Aprendiz-ADSI <aprendiz@sena.edu.co>
2019-04-04 19:58:07 -0500

    :white_check_mark:

--- Vistas/header.php
+++ Vistas/header.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Administrador</title>
+		<title>MiCarga</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
